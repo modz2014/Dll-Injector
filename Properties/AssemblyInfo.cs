eeed08a70e42ed2ba78e2d09d52c6ffa59f9c58a
modz2014 <dawgz1992.jp@gmail.com>
2021-12-12 23:51:03 +0800

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTitle("DLLInjector")]
+[assembly: AssemblyProduct("DLLInjector")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2013")]
+[assembly: ComVisible(false)]
+[assembly: Guid("988decf4-0c8d-4af6-868e-607fc3ae8827")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
